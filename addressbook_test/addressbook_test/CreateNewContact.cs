c3de2b9067f06ad928473af8a794dcc8291e6d9d
alespirin <aspirin160691@gmail.com>
2023-02-05 23:04:14 +0300

    add CreateNewContact

---
+++ addressbook_test/addressbook_test/CreateNewContact.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook_test
+{
+    [TestFixture]
+    public class Contact
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheContactTest()
+        {
+            driver.Navigate().GoToUrl("http://localhost/addressbook/index.php");
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Click();
+            // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=user | ]]
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Click();
+            // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=pass | ]]
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Click();
+            // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=pass | ]]
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            driver.FindElement(By.LinkText("add new")).Click();
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys("Alex");
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys("Spirin");
+            driver.FindElement(By.Name("mobile")).Click();
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys("89236502869");
+            driver.FindElement(By.Name("theform")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/form/input[21]")).Click();
+            driver.FindElement(By.XPath("//div[@id='nav']/ul/li")).Click();
+            driver.FindElement(By.LinkText("home")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
