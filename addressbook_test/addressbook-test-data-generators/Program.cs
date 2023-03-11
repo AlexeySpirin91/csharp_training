167f916c15e154d6be4acf6305ed0c86b967eeea
alespirin <aspirin160691@gmail.com>
2023-03-11 13:26:19 +0300

    add dsts generators csv

--- addressbook_test/addressbook-test-data-generators/Program.cs
+++ addressbook_test/addressbook-test-data-generators/Program.cs
@@ -13,14 +13,17 @@ namespace addressbook_test_data_generators
         {
             int count = Convert.ToInt32(args[0]);
             StreamWriter writer = new StreamWriter(args[1]);
+            string format = args[3];
             for(int i = 0; i < count; i++)
             {
-                writer.WriteLine(String.Format("{0},{1},{2}",
+                writer.WriteLine(String.Format("${0},${1},${2}",
                     TestBase.GenerateRandomString(10),
                     TestBase.GenerateRandomString(10),
                     TestBase.GenerateRandomString(10)));
             }
             writer.Close();
         }
+
+        
     }
 }
