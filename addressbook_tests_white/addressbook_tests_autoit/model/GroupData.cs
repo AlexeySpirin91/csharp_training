d2e92aac1c9fa50fa42a809301a970cbe295b175
alespirin <aspirin160691@gmail.com>
2023-03-31 17:30:08 +0300

    add windows test

---
+++ addressbook_tests_white/addressbook_tests_autoit/model/GroupData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace addressbook_tests_white
+{
+	public class GroupData : IComparable<GroupData>, IEquatable<GroupData>
+	{
+		public GroupData()
+		{
+		}
+
+		public string Name { get; set; }
+
+        public int CompareTo(GroupData? other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public bool Equals(GroupData? other)
+        {
+            return this.Name.Equals(other.Name);
+        }
+    }
+}
+
---
+++ addressbook_tests_white/addressbook_tests_autoit/tests/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_white
+{
+    
+    public class TestBase
+	{
+		public TestBase()
+		{
+		}
+
+		public ApplicationManager app;
+
+		[SetUp]
+        public void initApplication()
+		{
+			app = new ApplicationManager();
+		}
+
+		[TearDown]
+		public void stoppApplication()
+		{
+			app.Stop();
+		}
+	}
+}
+
